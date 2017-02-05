d78dc4392ca0feb9bb8151bd070cc4c49632800a
Adrian Banasiak <adrian.banasiak@consileon.pl>
2017-02-05 17:47:47 +0100

    Working APP

---
+++ app.js
@@ -0,0 +1,13 @@
+var http = require('http')
+var express = require('express')
+var app = express()
+var config = require('./lib/config')
+var db = require('./lib/db')
+var link = require('./lib/link')
+
+db.connect(function(){
+  http.createServer( app ).listen( config.port )
+
+  app.get('/new/*', link.prepare)
+  app.get('/:short_id', link.redirect)
+})
